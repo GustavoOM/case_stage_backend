f6d070a2aa85a943e6e5f7ab4dba355e922237aa
GustavoOM <gumartins2001@gmail.com>
2025-03-18 15:32:55 -0300

    Correcting errors

--- knexfile.ts
+++ knexfile.ts
@@ -2,16 +2,16 @@ import 'dotenv/config';
 import { Knex } from 'knex';
 
 const production: Knex.Config = {
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL, // URL do banco de dados
-    ssl: { rejectUnauthorized: false }, // Habilita SSL
-  },
-  migrations: {
-    extension: 'ts',
-    directory: './db/migrations',
-  },
-};
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL, // URL do banco de dados
+      ssl: { rejectUnauthorized: false }, // Habilita SSL
+    },
+    migrations: {
+      extension: 'ts',
+      directory: './db/migrations',
+    },
+  };
 
 const development: Knex.Config = {
   client: 'sqlite3',

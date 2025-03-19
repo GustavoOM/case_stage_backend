bcba35a2bf08a880c06fc6b15633baff5df18372
GustavoOM <gumartins2001@gmail.com>
2025-03-19 12:00:50 -0300

    Correcting errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { routes } from "./routes";
 
-export const app = Fastify()
+export const app = Fastify();
 
-app.register(routes)
+app.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+});
+
+app.register(routes);
